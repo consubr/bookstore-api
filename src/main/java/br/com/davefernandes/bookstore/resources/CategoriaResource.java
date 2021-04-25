74482dc213da5369622fdb90501813a57fd4aa61
consubr <consubr@yahoo.com.br>
2021-04-25 09:40:37 -0300

    Crio a Categoria com o status created e trago o link para busca no body

--- src/main/java/br/com/davefernandes/bookstore/resources/CategoriaResource.java
+++ src/main/java/br/com/davefernandes/bookstore/resources/CategoriaResource.java
@@ -1,15 +1,18 @@
 package br.com.davefernandes.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.davefernandes.bookstore.domain.Categoria;
 import br.com.davefernandes.bookstore.dtos.CategoriaDTO;
@@ -28,11 +31,19 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	//Trago todos e exibo somente os campos desejados no body com DTO
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-
+	
+	//Crio a Categoria com o status created e trago o link para busca no body
+	@PostMapping()
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
